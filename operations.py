8ae62a78e0573a3bc5baef7904d2c1158f2fa091
Samvel Hovhannisyan <samvelh@kth.se>
2025-02-06 15:03:10 +0100

    Add basics

---
+++ operations.py
@@ -0,0 +1,8 @@
+import math
+
+def add(x, y): return x + y 
+
+def mul(x, y): return x * y
+
+def sub(x, y): return x - y
+
---
+++ test_operations.py
@@ -0,0 +1,9 @@
+from operations import *
+import pytest
+
+def test_add(): assert add(3,2) == 5
+
+def test_mul(): assert mul(3,2) == 6
+
+def test_sub(): assert sub(3,2) == 1
+ 
